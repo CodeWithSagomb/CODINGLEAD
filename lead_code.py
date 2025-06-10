5fb9d0fc33f889187300d691acf400e637d3baa4
CHRIST sagombaye <mbayechriston@gmail.com>
2025-06-10 21:22:25 +0100

    First commit : adding my coding club files

--- lead_code.py
+++ lead_code.py
@@ -3,8 +3,13 @@
 lead_code.py
 
 '''
+from typing import List
 
 
 class LeadCode:
-
-    pass
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
